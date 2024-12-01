b6276ee3560fb724bf074bcb051065f52e6e8fb5
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-12-02 00:05:51 +0200

    Add books controller CRUD actions

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,8 +1,44 @@
 class Api::V1::BooksController < ApplicationController
- before_action :authenticate_user!, :only [:new, :create]
+  before_action :authenticate_user!, only: [ :create, :update, :destroy ]
 
- def index
-   books = Book.all
-    render json: books
- end
+  def index
+    books = Book.includes(:recommender)
+    render json: BookSerializer.new(books).serializable_hash
+  end
+
+  def show
+    book = Book.find(params[:id])
+    render json: BookSerializer.new(book, include: [ :recommender ]).serializable_hash
+  end
+
+  def create
+    book = current_user.recommended_books.build(book_params)
+
+    if book.save
+      render json: BookSerializer.new(book).serializable_hash, status: :created
+    else
+      render json: book.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    book = Book.find(params[:id])
+
+    if book.update(book_params)
+     render json: BookSerializer.new(book).serializable_hash
+    else
+      render json: book.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :description)
+  end
 end
